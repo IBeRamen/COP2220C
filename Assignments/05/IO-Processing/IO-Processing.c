78bdd82103c408165138a7fa2a20a97790410f4d
IBeRamen <iberamen@gmail.com>
2017-02-06 02:52:06 -0500

    ugh

--- Assignments/05/IO-Processing/IO-Processing.c
+++ Assignments/05/IO-Processing/IO-Processing.c
@@ -2,13 +2,18 @@
 *	
 */
 
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define CLS system("cls");
 #define PAUSE system("pause");
+#define FLUSH flush();
+#define SIZE 10
 
 void displayMenu();
+void flush();
 
 int getSelection();
 
@@ -16,6 +21,8 @@ int main()
 {
 
 	int userSelection;
+
+	static int students[SIZE][4];
 
 	do
 	{
@@ -27,7 +34,27 @@ int main()
 		case 1:
 			CLS;
 
+			int studentID, exam1, exam2, exam3, exam4;
 
+			printf("Enter student ID: ");
+			scanf_s("%i", &studentID);
+			FLUSH;
+
+			printf("Enter exam #1 score: ");
+			scanf_s("%i", &exam1);
+			students[studentID][1] = exam1;
+
+			printf("Enter exam #2 score: ");
+			scanf_s("%i", &exam2);
+			students[studentID][2] = exam2;
+
+			printf("Enter exam #3 score: ");
+			scanf_s("%i", &exam3);
+			students[studentID][3] = exam3;
+
+			printf("Enter exam #4 score: ");
+			scanf_s("%i", &exam4);
+			students[studentID][4] = exam4;
 
 			PAUSE;
 
@@ -36,6 +63,18 @@ int main()
 		case 2:
 			CLS;
 
+			float sum = 0.0;
+
+			printf("Find the average for student #: ");
+			scanf_s("%i", &studentID);
+
+			for (int i = 0; i <= 4; i++)
+			{
+				sum += students[studentID][i];
+			}
+
+			printf("The average for student #%i is %.2f.\n", studentID, sum / 4);
+
 			PAUSE;
 
 			break;
@@ -43,12 +82,37 @@ int main()
 		case 3:
 			CLS;
 
+			int exam = 0;
+			sum = 0;
+
+			printf("Find the average for exam #: ");
+			scanf_s("%i", &exam);
+
+			for (int i = 0; i < SIZE; i++)
+			{
+				sum += students[i][exam];
+			}
+
+			printf("The average for exam #%i is %.2f.\n", exam, sum);
+
 			PAUSE;
 
 			break;
 
 		case 4:
 			CLS;
+
+			sum = 0;
+
+			for (int i = 0; i < SIZE; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					sum += students[i][j];
+				}
+			}
+
+			printf("The sum is %.2f.\n", sum / SIZE);
 
 			PAUSE;
 
@@ -61,16 +125,24 @@ int main()
 
 			break;
 
+		case 6:
+			CLS;
+
+			printf("Quitting...");
+
+			break;
+
 		default:
 
 			PAUSE;
 
 			break;
 		}
-	} while (userSelection != 5);
+	} while (userSelection != 6);
+
+	return 0;
 
 } // end main
-
 
 void displayMenu()
 {
@@ -78,17 +150,17 @@ void displayMenu()
 	CLS;
 
 	printf("================ Menu =================\n");
-	printf("1. Display the average exam scores for a student\n");
+	printf("1. Enter exam scores\n");
 	printf("2. Display the average exam scores for a student\n");
-	printf("3. Display the class average\n");
-	printf("4. Clear out the gradebook\n");
-	printf("5. Quit\n");
+	printf("3. Display the average exam scores by exam\n");
+	printf("4. Display the class average\n");
+	printf("5. Clear out the gradebook\n");
+	printf("6. Quit\n");
 	printf("=======================================\n");
 
 	printf("Enter your selection: ");
 
 } // end displayMenu()
-
 
 int getSelection()
 {
@@ -96,6 +168,12 @@ int getSelection()
 
 	displayMenu();
 	scanf_s("%i", &result);
+	FLUSH;
 
 	return result;
 } // end getSelection()
+
+void flush()
+{
+	while (getchar() != '\n');
+} // end flush();
